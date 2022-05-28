ed32b63d806b91d34b93eea8ec307d5b8bc87ed9
VT_AOE_91 <amartin80108@gmail.com>
2022-05-28 16:56:42 -0400

    Set initial input field value to equal local storage if local storage !== null

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-let saveBtnEl = $('#8AMbtn');
-let text8amEl = document.getElementById('8AMtext');
+let saveBtnEl = $('#8AMbtn'); //Declares the save button from HTML as a variable to be used in JavaScript
+let text8amEl = document.getElementById('8AMtext'); //Saves the text input into the HTML form as a variable to be used in JavaScript
 
-// $( document ).ready(function() {
-//     if (text8amEl == null) {text8amEl = localStorage.getItem("8AM")}
-// });
+text8amEl.value = localStorage.getItem("8AM"); //Initializes given time form input field to local stroage value for same given time
 
-saveBtnEl.on('click', function() {
+saveBtnEl.on('click', function() { //On press of save button, saves input to local storage for given time and reports success to console
     console.log("8AM Btn Press")
     console.log("8AM text saved")
     localStorage.setItem("8AM",text8amEl.value)
